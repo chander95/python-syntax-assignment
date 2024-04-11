98c2a02dc7b148994c8cdfb1ab6beee0da7e227b
Alex Salmeron <juanasalmeron95@gmail.com>
2024-04-10 22:24:39 -0400

    Second commit with assignment one complted

---
+++ python-syntax-assignment.py
@@ -0,0 +1,29 @@
+#1 Showcase your Dance Moves!
+
+#TASK 1
+
+#code given:
+
+# weather = "sunny"
+# if weather == "sunny":
+# print("Wear sunglasses!")
+# else:
+# print("Take an umbrella!")
+
+#code correction
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+#TASK 2
+
+response = input("How do you feel today? ")
+
+if response == "happy" or response == "Happy":
+    print("That's great to hear!")
+elif response == "sad" or response == "Sad":
+    print("I hope your day gets better!")
