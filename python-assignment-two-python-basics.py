9caddad8d386310be7d369b03b831f5bd4046d3a
Alex Salmeron <juanasalmeron95@gmail.com>
2024-04-10 22:30:56 -0400

    Question 2 completed

---
+++ python-assignment-two-python-basics.py
@@ -0,0 +1,15 @@
+#Question 2
+
+#Task 1: Code Correction
+
+#Code provided
+# Pi_value = 3.14
+# userAge = 25
+# user_location = "New York"
+# MAXLIMIT = 1000
+
+#code corrected
+PI = 3.14
+user_age = 25
+user_location = "New York"
+MAX_LIMIT = 1000
