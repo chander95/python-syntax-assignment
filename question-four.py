21de9e48f017007e30b88b8ac54da7dfd830fca6
Alex Salmeron <juanasalmeron95@gmail.com>
2024-04-10 23:57:46 -0400

    Question 4 Completed

---
+++ question-four.py
@@ -0,0 +1,21 @@
+#Question 4
+
+#Task 1: Grocery Store Math
+bread = 6.99
+peanut_butter = 5.99
+jelly = 29.99
+
+total_cost = bread + peanut_butter + jelly
+print(total_cost)
+
+#Task 2: Bank Interest
+initial_balance = 10000
+interest_rate = 0.07
+year = 12
+#apr = (interest_rate * year) #* 100
+#print(apr)
+apr = (initial_balance * interest_rate) + initial_balance
+print(int(apr))
+
+
+
