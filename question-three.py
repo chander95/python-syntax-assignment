15b9e5f7fe6fd815ad90589226b0b04573c5685e
Alex Salmeron <juanasalmeron95@gmail.com>
2024-04-10 23:34:28 -0400

    question 3

---
+++ question-three.py
@@ -0,0 +1,19 @@
+#question 3
+
+#task 1
+
+#code given
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+#code updated
+variable_a = "Hello, World!"
+print(type(variable_a))
+variable_b = 23
+print(type(variable_b))
+variable_c = 3.14
+print(type(variable_c))
+variable_d = True
+print(type(variable_d))
